f13dcdd23b274f7ac5d73567372af5949bb2c290
Zaur Molotnikov <qutorial@gmail.com>
2019-02-25 15:01:25 +0100

    Bug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :update, :destroy]
-  before_action :authenticate_user!
+#  before_action :authenticate_user!
 
   # GET /recipes
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :recipes
+  authenticate :user do
+    resources :recipes
+  end
   
   mount_devise_token_auth_for 'User', at: 'auth'
 end
